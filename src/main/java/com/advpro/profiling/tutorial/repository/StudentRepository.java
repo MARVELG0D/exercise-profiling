c7045c5b9a5cd397c4b6b9a25b9d6f5ea7eb9e4c
MARVELG0D <bukan.marvel69@gmail.com>
2026-04-28 19:05:37 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGPA & joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -4,9 +4,13 @@ import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // Mengambil satu student dengan GPA tertinggi
+    Optional<Student> findFirstByOrderByGpaDesc();
 }
